dba442e0dc5e858b595270dc1cc68f1b3fe407b1
pieromarini <piero.marini21@gmail.com>
2019-09-27 12:54:04 -0500

    Added random changes in temperature

--- design_observer/src/utec/cs/cs2901/design/Main.java
+++ design_observer/src/utec/cs/cs2901/design/Main.java
@@ -8,13 +8,9 @@ public class Main {
     ClimateUI sensor3 = new ClimateUI();
 
     sensors.addSensor(sensor);
-    sensors.setTemperature(25);
-    sensors.setTemperature(10);
     sensors.addSensor(sensor2);
     sensors.addSensor(sensor3);
-    sensors.setTemperature(12);
-    sensors.setTemperature(12);
-    sensors.setTemperature(26);
-    sensors.setTemperature(19);
+
+    sensors.start();
   }
 }
